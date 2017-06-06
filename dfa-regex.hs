cf46cf5c650431ebee0c32e4386d9cbeb122190c
dramforever <dramforever@live.com>
2017-06-06 20:52:05 +0800

    Some refactoring makes things easier.
    Works on the first try, again! Thank to Haskell.

--- dfa-regex.hs
+++ dfa-regex.hs
@@ -4,26 +4,56 @@ module DfaRegex where
 
 import Data.Array
 import Data.List
+
 data Regex
     = C_0 | C_1
-    | Epsilon
-    | None
-    | Regex `Or` Regex
-    | Regex `Then` Regex
+    | Or [Regex]
+    | Concat [Regex]
     | Star Regex
+
+epsilon, none :: Regex
+epsilon = Concat []
+none = Or []
+
+mkOr :: [Regex] -> Regex
+mkOr as = case as >>= expandOr of
+    [a] -> a
+    rs -> Or rs
+    where
+        expandOr (Or as) = as
+        expandOr a = [a]
+
+mkStar :: Regex -> Regex
+mkStar (Or []) = epsilon
+mkStar (Concat []) = epsilon
+mkStar a = Star a
+
+mkConcat :: [Regex] -> Regex
+mkConcat as = case as >>= expandConcat of
+    as | any isNone as -> Or []
+    [a] -> a
+    as -> Concat as
+    where
+        expandConcat (Concat as) = as
+        expandConcat a = [a]
+
+        isNone (Or []) = True
+        isNone _ = False
+
 
 instance Show Regex where
     show regex = "/^" ++ go regex ++ "$/"
         where
             go C_0 = "0"
             go C_1 = "1"
-            go Epsilon = ""
-            go None = "-"
-            go a@Or{} = "(" ++ intercalate "|" (go <$> getOr a) ++ ")"
+            go (Or []) = "-"
+            go (Or as) = "(" ++ intercalate "|" (go <$> as) ++ ")"
+            go (Concat as) = w as
                 where
-                    getOr (a `Or` b) = getOr a ++ getOr b
-                    getOr a = [a]
-            go (a `Then` b) = go a ++ go b
+                    w (C_0 : Star C_0 : xs) = "0+" ++ w xs
+                    w (Star C_0 : C_0 : xs) = "0+" ++ w xs
+                    w (x : xs) = go x ++ w xs
+                    w [] = ""
             go (Star m) = optParens m ++ "*"
 
             optParens :: Regex -> String
@@ -38,28 +68,6 @@ infix 2 -->
 (-->) :: a -> b -> (a, b)
 (-->) = (,)
 
-simplify :: Regex -> Regex
-simplify regex = go regex
-    where
-        go (a `Then` b) =
-            case (go a, go b) of
-                (Epsilon, v) -> v
-                (u, Epsilon) -> u
-                (None, _) -> None
-                (_, None) -> None
-                (u, v) -> u `Then` v
-        go (a `Or` b) =
-            case (go a, go b) of
-                (None, v) -> v
-                (u, None) -> u
-                (u, v) -> u `Or` v
-        go (Star a) =
-            case go a of
-                None -> Epsilon
-                Epsilon -> Epsilon
-                u -> Star u
-        go r = r
-
 floyd :: Matrix Regex -> Matrix Regex
 floyd mat = foldl' update mat ixs
     where
@@ -67,19 +75,13 @@ floyd mat = foldl' update mat ixs
         pairs = indices mat
         ixs = [minIx .. maxIx]
 
-        -- matWithEps = listArray bnds $ go <$> pairs
-        --     where
-        --         go (i, j)
-        --             | i == j = (mat ! (i, j)) `Or` Epsilon
-        --             | otherwise = mat ! (i, j)
-
         update m k = listArray bnds $ (\(i, j) -> build m i j k) <$> pairs
 
-        build m i j k = (i ~> j) `Or` ((i ~> k) `Then` Star (k ~> k) `Then` (k ~> j))
+        build m i j k = mkOr [i ~> j, mkConcat [i ~> k, mkStar (k ~> k), k ~> j]]
             where u ~> v = m ! (u, v)
 
 makeMatrix :: (Int, Int) -> [((Int, Int), Regex)] -> Matrix Regex
-makeMatrix (minIx, maxIx) = accumArray Or None ((minIx, minIx), (maxIx, maxIx))
+makeMatrix (minIx, maxIx) ts = mkOr <$> accumArray (flip (:)) [] ((minIx, minIx), (maxIx, maxIx)) ts
 
 divisibleDfa :: Int -> [((Int, Int), Regex)]
 divisibleDfa m = [0 .. m - 1] >>= (\i ->
@@ -90,4 +92,4 @@ divisibleRegex :: Int -> Regex
 divisibleRegex n = (! (0, 0)) . floyd . makeMatrix (0, n - 1) $ divisibleDfa n
 
 divisibleBy7 :: String
-divisibleBy7 = show . simplify $ divisibleRegex 7
+divisibleBy7 = show (divisibleRegex 7)
